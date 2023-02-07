2e7badb6c3077e93ad4bcc38d95ba175632e957a
Palak Mishra <94302718+Mishra-Palak@users.noreply.github.com>
2023-02-08 01:01:44 +0530

    Update Lab4.c

--- Lab4Assignment/Lab4.c
+++ Lab4Assignment/Lab4.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <limits.h>
 #include <time.h>
-#include <math.h>
 
 //Recursive Quick Sort
 int rearrange(int A[], int start, int end) {

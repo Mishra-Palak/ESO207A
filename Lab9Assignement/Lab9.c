5a2ad3220e0b0b8b06b3487ecaa60cac6f109704
Palak Mishra <94302718+Mishra-Palak@users.noreply.github.com>
2023-04-14 22:29:13 +0530

    Update Lab9.c

--- Lab9Assignement/Lab9.c
+++ Lab9Assignement/Lab9.c
@@ -2,185 +2,330 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef struct node {
-    int key;
-    int rank;
-    struct node* left;
-    struct node* right;
-} Node;
-
-Node* createNode(int key, int rank) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->key = key;
-    newNode->rank = rank;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
-}
-
-int rankOf(Node* node) {
-    if (node == NULL)
-        return 0;
-    return node->rank;
-}
-
-void updateRank(Node* node) {
-    node->rank = rankOf(node->left) + 1;
-}
-
-Node* rightRotate(Node* x) {
-    Node* y = x->left;
-    Node* z = y->right;
-    y->right = x;
-    x->left = z;
-    updateRank(x);
-    updateRank(y);
-    return y;
-}
-
-Node* leftRotate(Node* x) {
-    Node* y = x->right;
-    Node* z = y->left;
-    y->left = x;
-    x->right = z;
-    updateRank(x);
-    updateRank(y);
-    return y;
-}
-
-Node* balanceNode(Node* node) {
-    if (rankOf(node->left) - rankOf(node->right) > 1) {
-        if (rankOf(node->left->left) >= rankOf(node->left->right))
-            node = rightRotate(node);
-        else {
-            node->left = leftRotate(node->left);
-            node = rightRotate(node);
-        }
-    }
-    else if (rankOf(node->right) - rankOf(node->left) > 1) {
-        if (rankOf(node->right->right) >= rankOf(node->right->left))
-            node = leftRotate(node);
-        else {
-            node->right = rightRotate(node->right);
-            node = leftRotate(node);
-        }
-    }
-    return node;
-}
-
-Node* insert(Node* root, int key) {
-    if (root == NULL)
-        return createNode(key, 1);
-    if (key < root->key)
-        root->left = insert(root->left, key);
-    else
-        root->right = insert(root->right, key);
-    updateRank(root);
-    return balanceNode(root);
-}
-
-Node* findMinNode(Node* node) {
-    if (node->left == NULL)
-        return node;
-    return findMinNode(node->left);
-}
-
-Node* deleteMinNode(Node* node) {
-    if (node->left == NULL)
-        return node->right;
-    node->left = deleteMinNode(node->left);
-    updateRank(node);
-    return balanceNode(node);
-}
-
-Node* delete(Node* root, int key) {
-    if (root == NULL)
-        return NULL;
-    if (key < root->key)
-        root->left = delete(root->left, key);
-    else if (key > root->key)
-        root->right = delete(root->right, key);
-    else {
-        Node* leftChild = root->left;
-        Node* rightChild = root->right;
-        free(root);
-        if (rightChild == NULL)
-            return leftChild;
-        Node* minNode = findMinNode(rightChild);
-        minNode->right = deleteMinNode(rightChild);
-        minNode->left = leftChild;
-        updateRank(minNode);
-        return balanceNode(minNode);
-    }
-    updateRank(root);
-    return balanceNode(root);
-}
-
-Node* search(Node* root, int key) {
-    if (root == NULL || root->key == key)
-        return root;
-    if (key < root->key)
-        return search(root->left, key);
-    else
-        return search(root->right, key);
-}
-
-void insertRandomNumbers(Node** root) {
+void randomNumberGenerator(int N) {
+    FILE * random = fopen("random.txt", "w");
+    
     srand(time(0));
-    for (int i = 0; i < 500; i++) {
-        int num = 1 + rand() % 10000; //1 to 10000
-        *root = insert(*root, num);
+    for (int i = 0; i < N; i++) {
+        int x = 1+ rand()%1000; //1 to 1000
+        fprintf(random, "%d ", x);
+    }
+    
+    fclose(random);
+}
+
+enum COLOR {Red, Black};
+
+typedef struct tree_node {
+    int data;
+    struct tree_node *right;
+    struct tree_node *left;
+    struct tree_node *parent;
+    enum COLOR color;
+} tree_node;
+
+typedef struct red_black_tree {
+    tree_node *root;
+    tree_node *NIL;
+} red_black_tree;
+
+tree_node* new_tree_node(int data) {
+    tree_node* n = malloc(sizeof(tree_node));
+    n->left = NULL;
+    n->right = NULL;
+    n->parent = NULL;
+    n->data = data;
+    n->color = Red;
+
+    return n;
+}
+
+red_black_tree* new_red_black_tree() {
+    red_black_tree *t = malloc(sizeof(red_black_tree));
+    tree_node *nil_node = malloc(sizeof(tree_node));
+    nil_node->left = NULL;
+    nil_node->right = NULL;
+    nil_node->parent = NULL;
+    nil_node->color = Black;
+    nil_node->data = 0;
+    t->NIL = nil_node;
+    t->root = t->NIL;
+
+    return t;
+}
+
+void left_rotate(red_black_tree *t, tree_node *x) {
+    tree_node *y = x->right;
+    x->right = y->left;
+    if(y->left != t->NIL) {
+        y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if(x->parent == t->NIL) { //x is root
+        t->root = y;
+    }
+    else if(x == x->parent->left) { //x is left child
+        x->parent->left = y;
+    }
+    else { //x is right child
+        x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
+}
+
+void right_rotate(red_black_tree *t, tree_node *x) {
+    tree_node *y = x->left;
+    x->left = y->right;
+    if(y->right != t->NIL) {
+        y->right->parent = x;
+    }
+    y->parent = x->parent;
+    if(x->parent == t->NIL) { //x is root
+        t->root = y;
+    }
+    else if(x == x->parent->right) { //x is left child
+        x->parent->right = y;
+    }
+    else { //x is right child
+        x->parent->left = y;
+    }
+    y->right = x;
+    x->parent = y;
+}
+
+void insertion_fixup(red_black_tree *t, tree_node *z) {
+    while(z->parent->color == Red) {
+        if(z->parent == z->parent->parent->left) { //z.parent is the left child
+
+            tree_node *y = z->parent->parent->right; //uncle of z
+
+            if(y->color == Red) { //case 1
+                z->parent->color = Black;
+                y->color = Black;
+                z->parent->parent->color = Red;
+                z = z->parent->parent;
+            }
+            else { //case2 or case3
+                if(z == z->parent->right) { //case2
+                    z = z->parent; //marked z.parent as new z
+                    left_rotate(t, z);
+                }
+                //case3
+                z->parent->color = Black; //made parent black
+                z->parent->parent->color = Red; //made parent red
+                right_rotate(t, z->parent->parent);
+            }
+        }
+        else { //z.parent is the right child
+            tree_node *y = z->parent->parent->left; //uncle of z
+
+            if(y->color == Red) {
+                z->parent->color = Black;
+                y->color = Black;
+                z->parent->parent->color = Red;
+                z = z->parent->parent;
+            }
+            else {
+                if(z == z->parent->left) {
+                    z = z->parent; //marked z.parent as new z
+                    right_rotate(t, z);
+                }
+                z->parent->color = Black; //made parent black
+                z->parent->parent->color = Red; //made parent red
+                left_rotate(t, z->parent->parent);
+            }
+        }
+    }
+    t->root->color = Black;
+}
+
+void insert(red_black_tree *t, tree_node *z) {
+    tree_node* y = t->NIL; //variable for the parent of the added node
+    tree_node* temp = t->root;
+
+    while(temp != t->NIL) {
+        y = temp;
+        if(z->data < temp->data)
+            temp = temp->left;
+        else
+            temp = temp->right;
+    }
+    z->parent = y;
+
+    if(y == t->NIL) { //newly added node is root
+        t->root = z;
+    }
+    else if(z->data < y->data) //data of child is less than its parent, left child
+        y->left = z;
+    else
+        y->right = z;
+
+    z->right = t->NIL;
+    z->left = t->NIL;
+
+    insertion_fixup(t, z);
+}
+
+void rb_transplant(red_black_tree *t, tree_node *u, tree_node *v) {
+    if(u->parent == t->NIL)
+        t->root = v;
+    else if(u == u->parent->left)
+        u->parent->left = v;
+    else
+        u->parent->right = v;
+    v->parent = u->parent;
+}
+
+tree_node* minimum(red_black_tree *t, tree_node *x) {
+    while(x->left != t->NIL)
+        x = x->left;
+    return x;
+}
+
+void rb_delete_fixup(red_black_tree *t, tree_node *x) {
+    while(x != t->root && x->color == Black) {
+        if(x == x->parent->left) {
+            tree_node *w = x->parent->right;
+            if(w->color == Red) {
+                w->color = Black;
+                x->parent->color = Red;
+                left_rotate(t, x->parent);
+                w = x->parent->right;
+            }
+            if(w->left->color == Black && w->right->color == Black) {
+                w->color = Red;
+                x = x->parent;
+            }
+            else {
+                if(w->right->color == Black) {
+                    w->left->color = Black;
+                    w->color = Red;
+                    right_rotate(t, w);
+                    w = x->parent->right;
+                }
+                w->color = x->parent->color;
+                x->parent->color = Black;
+                w->right->color = Black;
+                left_rotate(t, x->parent);
+                x = t->root;
+            }
+        } else {
+            tree_node *w = x->parent->left;
+            if(w->color == Red) {
+                w->color = Black;
+                x->parent->color = Red;
+                right_rotate(t, x->parent);
+                w = x->parent->left;
+            }
+            if(w->right->color == Black && w->left->color == Black) {
+                w->color = Red;
+                x = x->parent;
+            }
+            else {
+                if(w->left->color == Black) {
+                    w->right->color = Black;
+                    w->color = Red;
+                    left_rotate(t, w);
+                    w = x->parent->left;
+                }
+                w->color = x->parent->color;
+                x->parent->color = Black;
+                w->left->color = Black;
+                right_rotate(t, x->parent);
+                x = t->root;
+            }
+        }
+    }
+    x->color = Black;
+}
+
+void rb_delete(red_black_tree *t, tree_node *z) {
+    tree_node *y = z;
+    tree_node *x;
+    enum COLOR y_orignal_color = y->color;
+    if(z->left == t->NIL) {
+        x = z->right;
+        rb_transplant(t, z, z->right);
+    }
+    else if(z->right == t->NIL) {
+        x = z->left;
+        rb_transplant(t, z, z->left);
+    }
+    else {
+        y = minimum(t, z->right);
+        y_orignal_color = y->color;
+        x = y->right;
+        if(y->parent == z) {
+            x->parent = z;
+        }
+        else {
+            rb_transplant(t, y, y->right);
+            y->right = z->right;
+            y->right->parent = y;
+        }
+        rb_transplant(t, z, y);
+        y->left = z->left;
+        y->left->parent = y;
+        y->color = z->color;
+    }
+    if(y_orignal_color == Black)
+        rb_delete_fixup(t, x);
+}
+
+void inorder(red_black_tree *t, tree_node *n) {
+    if(n != t->NIL) {
+        inorder(t, n->left);
+        printf("%d ", n->data);
+        inorder(t, n->right);
     }
 }
 
-void findAndDeleteMin(Node** root) {
-    for (int i = 0; i < 100; i++) {
-        Node* minNode = findMinNode(*root);
-        printf("Minimum item: %d\n", minNode->key);
-        *root = delete(*root, minNode->key);
+tree_node* search(red_black_tree *t, int key) {
+    tree_node *y = t->root;
+    while (y != t->NIL && key != y->data) {
+        if (key < y->data) y = y->left;
+        else y = y->right;
     }
-}
-
-void inorder(Node* root) {
-    if(root != NULL) {
-        inorder(root->left);
-        printf("%d ", root->key);
-        inorder(root->right);
-    }
+    return y;
 }
 
 int main() {
-    Node* root = NULL;
+    red_black_tree *t = new_red_black_tree();
+
     int choice;
-    printf("-----RB TREE-----\n");
+    printf("-----RED BLACK TREE-----\n");
     printf("Enter your choice: \n1. Insert\n2. Search\n3. Delete\n4. Inorder\n5. Exit\n");
     scanf("%d", &choice);
     
     while(choice != 5) {
         if (choice == 1) {
+            tree_node *a;
             int item;
             printf("Enter the item: ");
             scanf("%d", &item);
-            root = insert(root, item);
+            a = new_tree_node(item);
+            insert(t, a);
         } else if (choice == 2) {
             int item;
             printf("Enter the item: ");
             scanf("%d", &item);
-            Node* t = search(root, item);
-            if (t != NULL) {
+            tree_node* a = search(t, item);
+            if (a->data == item) {
                 printf("Item found.\n");
-            } else printf("Item not found!\n");
+            }
+            else printf("Item not found!\n");
         } else if (choice == 3) {
+            tree_node *a;
             int item;
             printf("Enter the item: ");
             scanf("%d", &item);
-            Node* t = search(root, item);
-            if (t != NULL) {
-                root = delete(root, item);
-            } else printf("Item not found!\n");
+            a = search(t, item);
+            if (a->data == item) rb_delete(t, a);
+            else printf("Item not found!\n");
         }
         else if (choice == 4) {
-            inorder(root);
+            inorder(t, t->root);
             printf("\n");
         }
         else if (choice == 5) break;
@@ -191,9 +336,27 @@ int main() {
     }
 
     printf("\n-----SECOND TASK-----\n");
-    root = NULL;
-    insertRandomNumbers(&root);
-    inorder(root);
-    findAndDeleteMin(&root);
-    inorder(root);
+    red_black_tree *r = new_red_black_tree();
+    randomNumberGenerator(500);
+
+    FILE * random = fopen("random.txt", "r");
+    for (int i = 0; i < 500; i++) {
+        int item;
+        fscanf(random, "%d", &item);
+        tree_node *a = new_tree_node(item);
+        insert(r, a);
+    }
+    fclose(random);
+
+    inorder(r, r->root);
+    printf("\n");
+
+    for (int i = 0; i < 100; i++) {
+        tree_node* a = minimum(r, r->root);
+        printf("Deleted: %d\n", a->data);
+        rb_delete(r, a);
+    }
+
+    inorder(r, r->root);
+    printf("\n");
 }
